85a3c83e732a6210c455f04f3e5f6cd814074ae3
David Castrillón <david.castri@hotmail.es>
2021-09-02 17:29:15 +0200

    02/09/2021 - Ejemplo de prueba en app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,40 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+
+  /* Properties */
   title = 'angular-qr-code-generator';
+  
+  public values: string = "";
+  public level: "L" | "M" | "Q" | "H";
+  public width: number;
+  
+  /* Constructor */
+  constructor() {
+    this.level = "L"; 
+    /* 
+      Mainly used for QR Correction level. How much error correction may be required:
+        Level L – up to 7% damage
+        Level M – up to 15% damage
+        Level Q – up to 25% damage
+        Level H – up to 30% damage
+    */
+    this.values = "QR code string value";
+    this.width = 200;
+  }
+
+  /* Methods */
+  qrLevel(val: "L" | "M" | "Q" | "H") {
+    this.level = val;
+  }
+
+  qrData(val: string) {
+    this.values = val;
+  }
+
+  qrWidth(val: number) {
+    this.width = val;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,12 @@
+/* Angular Imports */
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
+/* Third Party Imports */
+import { QRCodeModule } from 'angular2-qrcode';
+
+/* App Imports */
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +14,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    /* Third Party */
+    QRCodeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
